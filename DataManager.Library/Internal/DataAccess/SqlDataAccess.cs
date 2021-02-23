18dafedfa5a1af576ad40227044f57e52c7d6923
Filip Skukan <skukan159@gmail.com>
2021-02-23 13:16:19 +0100

    Added trasaction handling in data access

--- DataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ DataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -5,19 +5,19 @@ using System.Data.SqlClient;
 using Dapper;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
 
 namespace DataManager.Library.Internal.DataAccess
 {
-    internal class SqlDataAccess
+    internal class SqlDataAccess : IDisposable
     {
-        /*  public string GetConnectionString(string name)
-          {
-              return ConfigurationManager.ConnectionStrings[name].ConnectionString;
-          }*/
+
+        private IDbConnection _connection;
+        private IDbTransaction _transaction;
+
 
         public async Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionString)
         {
-            //string connectionString = GetConnectionString(connectionStringName);
             using IDbConnection cnn = new SqlConnection(connectionString);
             var result = await cnn
                 .QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
@@ -31,6 +31,48 @@ namespace DataManager.Library.Internal.DataAccess
             //string connectionString = GetConnectionString(connectionStringName);
             using IDbConnection cnn = new SqlConnection(connectionString);
             await cnn.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+
+        public void StartTransaction(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+
+            _transaction = _connection.BeginTransaction();
+        }
+
+        public async Task SaveDataInTransaction<T>(string storedProcedure, T parameters)
+        {
+            await _connection
+                .ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure, transaction: _transaction);
+        }
+
+        public async Task<List<T>> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
+        {
+            var result = await _connection
+                .QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure, transaction: _transaction);
+
+            List<T> rows = result.ToList();
+
+            return rows;
+        }
+
+        public void CommitTransaction()
+        {
+            _transaction?.Commit();
+            _connection?.Close();
+        }
+
+        public void RollbackTransaction()
+        {
+            _transaction?.Rollback();
+            _connection?.Close();
+        }
+
+        public void Dispose()
+        {
+            CommitTransaction();
         }
     }
 }
